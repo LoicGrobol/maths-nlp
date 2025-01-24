6635aade17a67847f682e1fb3f473fee21e0bc12
L. Grobol <loic.grobol@gmail.com>
2025-01-24 13:03:18 +0100

    add jupyterlite (not used yet)

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# maths-nlp
+Mathématiques pour le TAL
+=========================
 
 Contenus pour le cours « Mathématiques pour le TAL » du master Plurital 
+
+
+## How do I build this
+
+(In a virtual env)
+
+```bash
+uv pip install -U -r requirements.txt
+jupyter-book build maths-nlp --path-output .
+```
+
+To serve, watch and rebuild:
+
+```bash
+python tools/serve.py maths-nlp
+```
--- tools/serve.py
+++ tools/serve.py
@@ -1,6 +1,8 @@
+import glob
 import pathlib
 import click
 from livereload import Server, shell
+import pathspec
 
 
 @click.command(help="Watch and serve the book")
@@ -15,6 +17,15 @@ from livereload import Server, shell
     type=click.Path(writable=True, file_okay=False, path_type=pathlib.Path),
 )
 def serve(book_path: pathlib.Path, path_output: pathlib.Path):
+    # TODO: find a way to reload the patterns if the ignore file changes
+    # TODO: support nested gitignores
+    if (ignores_file := book_path / ".gitignore").exists():
+        with open(ignores_file) as in_stream:
+            ps = pathspec.PathSpec.from_lines("gitwildmatch", in_stream)
+        ignore = ps.match_file
+    else:
+        ignore = None
+
     cmd_args = [
         "jupyter-book",
         "build",
@@ -24,7 +35,7 @@ def serve(book_path: pathlib.Path, path_output: pathlib.Path):
     ]
     shell([*cmd_args, "--all"])()
     server = Server()
-    server.watch(book_path, shell(cmd_args))
+    server.watch(book_path, shell(cmd_args), delay=1, ignore=ignore)
     server.serve(root=path_output / "_build/html")
 
 
